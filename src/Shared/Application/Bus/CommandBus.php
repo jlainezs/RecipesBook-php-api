7c0563ce7578822888ea9017a9460d9843c58acb
Pep Lainez <jlainezs@gmail.com>
2026-06-24 11:01:13 +0200

    Introduce `CommandBus` interface and Symfony implementation in Shared module

---
+++ src/Shared/Application/Bus/CommandBus.php
@@ -0,0 +1,6 @@
+<?php
+namespace App\Shared\Application\Bus;
+interface CommandBus
+{
+    public function dispatch(object $command): void;
+}
---
+++ src/Shared/Infrastructure/Bus/SymfonyCommandBus.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Shared\Infrastructure\Bus;
+
+use App\Shared\Application\Bus\CommandBus;
+use Symfony\Component\Messenger\Exception\ExceptionInterface;
+use Symfony\Component\Messenger\MessageBusInterface;
+
+readonly class SymfonyCommandBus implements CommandBus
+{
+    public function __construct(private MessageBusInterface $commandBus)
+    {}
+
+    /**
+     * @throws ExceptionInterface
+     */
+    public function dispatch(object $command): void
+    {
+        $this->commandBus->dispatch($command);
+    }
+}
