18f132365721658bdec68c6146712cbd558a50bb
Pep Lainez <jlainezs@gmail.com>
2026-07-14 06:53:20 +0200

    Recipe ingredient migration. Table creation, indexing and foreign keys.

---
+++ migrations/Version20260712205035.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260712205035 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE recipe_ingredient (quantity REAL NOT NULL, ordering INT NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, id UUID NOT NULL, recipe_id UUID DEFAULT NULL, ingredient_id UUID DEFAULT NULL, unit_of_measure_id UUID DEFAULT NULL, PRIMARY KEY (id))');
+        $this->addSql('CREATE INDEX IDX_22D1FE1359D8A214 ON recipe_ingredient (recipe_id)');
+        $this->addSql('CREATE INDEX IDX_22D1FE13933FE08C ON recipe_ingredient (ingredient_id)');
+        $this->addSql('CREATE INDEX IDX_22D1FE13DA4E2C90 ON recipe_ingredient (unit_of_measure_id)');
+        $this->addSql('ALTER TABLE recipe_ingredient ADD CONSTRAINT FK_22D1FE1359D8A214 FOREIGN KEY (recipe_id) REFERENCES recipe (id)');
+        $this->addSql('ALTER TABLE recipe_ingredient ADD CONSTRAINT FK_22D1FE13933FE08C FOREIGN KEY (ingredient_id) REFERENCES ingredient (id)');
+        $this->addSql('ALTER TABLE recipe_ingredient ADD CONSTRAINT FK_22D1FE13DA4E2C90 FOREIGN KEY (unit_of_measure_id) REFERENCES unit_of_measure (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE recipe_ingredient DROP CONSTRAINT FK_22D1FE1359D8A214');
+        $this->addSql('ALTER TABLE recipe_ingredient DROP CONSTRAINT FK_22D1FE13933FE08C');
+        $this->addSql('ALTER TABLE recipe_ingredient DROP CONSTRAINT FK_22D1FE13DA4E2C90');
+        $this->addSql('DROP TABLE recipe_ingredient');
+    }
+}
