48c358f1efca494482203c523aa3baf3e3af26b3
Pep Lainez <jlainezs@gmail.com>
2026-08-06 19:18:50 +0200

    Use a DTO to create a shopping list.

---
+++ src/ShoppingList/Application/Command/ShoppingListCreate/ShoppingListCreateDto.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\ShoppingList\Application\Command\ShoppingListCreate;
+
+use Symfony\Component\Validator\Constraints as Assert;
+final readonly class ShoppingListCreateDto
+{
+    public function __construct(
+        #[Assert\NotBlank]
+        public string $name,
+    ){}
+}
--- src/ShoppingList/Presentation/Http/Controller/CreateShoppingListController.php
+++ src/ShoppingList/Presentation/Http/Controller/CreateShoppingListController.php
@@ -4,9 +4,11 @@ namespace App\ShoppingList\Presentation\Http\Controller;
 use App\Shared\Application\Bus\CommandBus;
 use App\Shared\Application\Service\ApplicationDataValidator;
 use App\ShoppingList\Application\Command\ShoppingListCreate\ShoppingListCreateCommand;
+use App\ShoppingList\Application\Command\ShoppingListCreate\ShoppingListCreateDto;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class CreateShoppingListController extends AbstractController
@@ -17,10 +19,12 @@ final class CreateShoppingListController extends AbstractController
     ){}
 
     #[Route('/api/v1/shopping-lists/create', name: 'shopping_lists_create', methods: ['POST'])]
-    public function  __invoke(Request $request): JsonResponse
+    public function  __invoke(
+        #[MapRequestPayload]
+        ShoppingListCreateDto $request
+    ): JsonResponse
     {
-        $name = $request->getPayload()->getString('name');
-        $cmd = new ShoppingListCreateCommand($name);
+        $cmd = new ShoppingListCreateCommand($request->name);
         $this->validator->validate($cmd);
         $this->commandBus->dispatch($cmd);
 
